9b7f41b1247b81aed041eafdd09ae005d8948ca9
Zachary Sturman <100522031+ZSturman@users.noreply.github.com>
2023-03-14 17:13:16 -0600

    changed to prod

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app, db
-from config import DevelopmentConfig, ProductionConfig, TestingConfig, get_settings
+from config import DevelopmentConfig, ProductionConfig, TestingConfig
 from pathlib import Path
 
-app = create_app(config_class=DevelopmentConfig)
-app.config.from_object('config.DevelopmentConfig')
+app = create_app(config_class=ProductionConfig)
+app.config.from_object('config.ProductionConfig')
 
 if __name__ == "__main__":
     app.run()
